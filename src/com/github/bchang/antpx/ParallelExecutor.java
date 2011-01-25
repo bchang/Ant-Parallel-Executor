0800eb968e2671451d3404f5429253b42833f265
bchang <bchanged@gmail.com>
2011-01-25 14:26:58 -0800

    added debug logging option (antpx.debug) added timeout option (antpx.timeout)
    User targets from the command line are run independently.
    
    In the event of target failures, the first one gets reported. Concurrent targets are allowed to complete, but their failures don't get reported. Targets already submitted to the executor service are then not run.

--- src/com/github/bchang/antpx/ParallelExecutor.java
+++ src/com/github/bchang/antpx/ParallelExecutor.java
@@ -13,59 +13,102 @@ import java.util.concurrent.TimeUnit;
 
 /**
  */
+@SuppressWarnings({"UnusedDeclaration"})
 public class ParallelExecutor implements Executor {
 
     public static final int DEFAULT_NUM_THREADS = 1;
+    public static final int DEFAULT_TIMEOUT = 600;
     public static final String NUMTHREADS_PROPKEY = "antpx.numthreads";
+    public static final String DEBUG_PROPKEY = "antpx.debug";
+    public static final String TIMEOUT_PROPKEY = "antpx.timeout";
 
     private static final SingleCheckExecutor SUB_EXECUTOR = new SingleCheckExecutor();
 
-    private HashSet<String> targetsCompleted = new HashSet<String>();
-    private LinkedList<Target> targetsYetToRun;
-    ExecutorService executorService;
+    private Deque<String> userTargets;
+    private LinkedList<Target> targetsYetToRun = new LinkedList<Target>();
+    private HashSet<String> targetsCompleted;
+    private FailedTarget failedTarget;
+
+    private boolean debug;
+    private ExecutorService executorService;
+    private Project project;
 
     public Executor getSubProjectExecutor() {
         return SUB_EXECUTOR;
     }
 
     public void executeTargets(Project project, String[] targetNames) throws BuildException {
-        int numThreads = DEFAULT_NUM_THREADS;
-        String propNumThreads = project.getProperty(NUMTHREADS_PROPKEY);
-        if (propNumThreads != null) {
-            numThreads = Integer.parseInt(propNumThreads);
-        }
-        project.log(getClass().getName() + " with " + numThreads + " threads");
+        this.project = project;
+
+        debug = "true".equals(project.getProperty(DEBUG_PROPKEY));
+        int numThreads = readIntProp(project, DEFAULT_NUM_THREADS, NUMTHREADS_PROPKEY);
+        int timeout = readIntProp(project, DEFAULT_TIMEOUT, TIMEOUT_PROPKEY);
+
+        project.log(getClass().getName() + " with " + numThreads + " threads, timeout " + timeout + " sec");
         executorService = Executors.newFixedThreadPool(numThreads);
 
-        @SuppressWarnings({"unchecked"})
-        Vector<Target> topoSort = (Vector<Target>) project.topoSort(targetNames, project.getTargets(), false);
-        targetsYetToRun = new LinkedList<Target>(topoSort);
+        userTargets = new LinkedList<String>(Arrays.asList(targetNames));
         loadReadyTargets();
 
         try {
-            executorService.awaitTermination(600, TimeUnit.SECONDS);
+            executorService.awaitTermination(timeout, TimeUnit.SECONDS);
         }
         catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        synchronized(this) {
+            if (failedTarget != null) {
+                throw failedTarget.ex;
+            }
+        }
     }
 
     private synchronized void targetCompleted(String targetName) {
+        if (debug) {
+          project.log("TargetQueue: completed target " + targetName);
+        }
         targetsCompleted.add(targetName);
         loadReadyTargets();
     }
 
     private synchronized void loadReadyTargets() {
         if (targetsYetToRun.size() == 0) {
-            executorService.shutdown();
+            String userTargetName = userTargets.poll();
+            if (userTargetName == null) {
+                executorService.shutdown();
+            }
+            else {
+                @SuppressWarnings({"unchecked"})
+                Vector<Target> topoSort = (Vector<Target>) project.topoSort(userTargetName, project.getTargets(), false);
+                targetsYetToRun = new LinkedList<Target>(topoSort);
+                targetsCompleted = new HashSet<String>();
+            }
         }
         for (Iterator<Target> it = targetsYetToRun.iterator(); it.hasNext();) {
             Target target = it.next();
             if (isTargetReady(target, targetsCompleted)) {
                 it.remove();
+                if (debug) {
+                    project.log("TargetQueue: submitting target " + target.getName());
+                }
                 executorService.submit(new TargetJob(target));
             }
         }
+    }
+
+    private synchronized void targetFailed(String targetName, RuntimeException ex) {
+        if (failedTarget == null) {
+            failedTarget = new FailedTarget(targetName, ex);
+            executorService.shutdown();
+        }
+        else {
+            // ignore
+        }
+    }
+
+    private synchronized boolean hasATargetFailed() {
+        return failedTarget != null;
     }
 
     private static boolean isTargetReady(Target target, HashSet<String> completed) {
@@ -78,7 +121,12 @@ public class ParallelExecutor implements Executor {
         return true;
     }
 
-    class TargetJob implements Runnable {
+    private static int readIntProp(Project project, int defaultVal, String propKey) {
+        String prop = project.getProperty(propKey);
+        return prop == null ? defaultVal : Integer.parseInt(prop);
+    }
+
+    private class TargetJob implements Runnable {
         private Target target;
 
         TargetJob(Target target) {
@@ -86,13 +134,24 @@ public class ParallelExecutor implements Executor {
         }
 
         public void run() {
-            try {
-                target.performTasks();
-            } catch (BuildException e) {
-                // TODO - probably not quite right
-                executorService.shutdownNow();
+            if (!hasATargetFailed()) {
+                try {
+                    target.performTasks();
+                    targetCompleted(target.getName());
+                } catch (RuntimeException ex) {
+                    targetFailed(target.getName(), ex);
+                }
             }
-            targetCompleted(target.getName());
+        }
+    }
+
+    private static class FailedTarget {
+        String targetName;
+        RuntimeException ex;
+
+        private FailedTarget(String targetName, RuntimeException ex) {
+            this.targetName = targetName;
+            this.ex = ex;
         }
     }
 }
